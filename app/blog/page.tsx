a487b1399ef3aee1eccfc03a45f2032a4cb7d1c2
sargsyansevak <thesevaksargsyan@gmail.com>
2023-12-28 10:18:41 -0600

    blog

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,5 +1,14 @@
+import Blogs from "@/components/Blogs";
+import { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "Blogs",
+};
 const page = () => {
-  return <div>Blog</div>;
+  return (
+    <div className="mb-20 ">
+      <Blogs />
+    </div>
+  );
 };
 
 export default page;
--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,18 +1,11 @@
 import Card from "@/components/Card";
 import { Metadata } from "next";
+import Work from ".";
 export const metadata: Metadata = {
   title: "Work",
 };
 const page = () => {
-  return (
-    <div className="md:py-20 py-16 px-4 flex justify-center items-center">
-      <div className="w-full h-fit flex flex-col md:gap-[120px] gap-[40px]">
-        {[1, 2, 3, 4, 5].map((i) => (
-          <Card key={i} />
-        ))}
-      </div>
-    </div>
-  );
+  return <Work />;
 };
 
 export default page;
