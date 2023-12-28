2fc4638d5174ed041b46fb203f9dd32fde6d8cf3
sargsyansevak <thesevaksargsyan@gmail.com>
2023-12-28 13:47:16 -0600

    k

--- components/LanguageSwitcher.tsx
+++ components/LanguageSwitcher.tsx
@@ -1,6 +1,6 @@
 const LanguageSwitcher = () => {
   return (
-    <div className="flex items-center justify-center w-full h-full font-extrabold text-sm">
+    <div className="flex items-center justify-center w-full h-full font-extrabold text-sm cursor-pointer dark:text-white">
       ENG
     </div>
   );
