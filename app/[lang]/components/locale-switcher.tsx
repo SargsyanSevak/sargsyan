141fe75de564ef1140805325019599abbc5ac456
sargsyansevak <thesevaksargsyan@gmail.com>
2024-01-05 05:22:01 -0600

    kk

--- app/[lang]/components/locale-switcher.tsx
+++ app/[lang]/components/locale-switcher.tsx
@@ -33,11 +33,11 @@ export default function LocaleSwitcher({
               href={redirectedPathName(locale)}
               className={` ${
                 desktopView
-                  ? "bg-inherit text-black dark:text-white border-none hover:bg-gray-100 dark:hover:bg-[#1E5F74]  transition-all"
-                  : " text-white border"
+                  ? " dark:text-white border-none hover:bg-gray-100 dark:hover:bg-[#1E5F74]  transition-all"
+                  : " text-black border dark:text-white"
               } rounded-md   px-6 uppercase py-2 ${
                 locale === lang
-                  ? "bg-gray-300 hover:bg-gray-300 dark:bg-[#1E5F74] dark:hover:bg-[#1E5F74]"
+                  ? "bg-gray-300 hover:bg-gray-300   dark:bg-[#1E5F74] dark:hover:bg-[#1E5F74]"
                   : ""
               }`}
             >
